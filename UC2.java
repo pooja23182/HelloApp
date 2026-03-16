a6665af2a442663a6af5d42c5829848a11f51773
pooja23182 <pooja23vijayakumar@gmail.com>
2026-03-16 20:51:40 +0530

    Added files

---
+++ UC2.java
@@ -0,0 +1,11 @@
+public class UC2  {
+   public UC2() {
+   }
+
+   public static void main(String[] var0) {
+      String var1 = var0[0];
+      System.out.println("Hello, " + var1 + "!");
+   }
+
+    
+}
